7dc0ab26c10a0e1865d71d9fbf06ffbe91075b41
Konstantin Usoltsev <kostiiiikostiiiii@gmail.com>
2024-05-17 22:47:08 +0300

    fix: 403 with POST request

--- src/main/kotlin/ru/kosti/googledrivemanager/configuration/SecurityConfig.kt
+++ src/main/kotlin/ru/kosti/googledrivemanager/configuration/SecurityConfig.kt
@@ -1,20 +1,27 @@
 package ru.kosti.googledrivemanager.configuration
 
+import org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.disable
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
 
-//@Configuration
-//@EnableWebSecurity
-//class SecurityConfig {
-//    @Bean
-//    @Throws(Exception::class)
-//    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain? {
-//        http
-//            .authorizeHttpRequests { request ->
-//                request.anyRequest().permitAll()
-//            }
-//            .cors { disable() }
-//            .csrf { disable() }
-//            .httpBasic { disable() }
-//            .formLogin { disable() }
-//        return http.build()
-//    }
-//}
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain? {
+        http
+            .authorizeHttpRequests { request ->
+                request.anyRequest().permitAll()
+            }
+            .cors { disable() }
+            .csrf().disable()
+            .httpBasic { disable() }
+            .formLogin { disable() }
+        return http.build()
+    }
+}
